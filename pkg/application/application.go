9dfd1256f7c385071e65771874e14597bfd23fa6
Amry Amanah <amry.amanah@outlook.com>
2021-01-03 00:30:49 +0700

    remove unused function in pkg/application and fix docker-compose

--- pkg/application/application.go
+++ pkg/application/application.go
@@ -1,22 +1,12 @@
 package application
 
 import (
-	"database/sql"
 	"fmt"
 	store "github.com/amryamanah/go-boilerplate/internal/store/sqlc"
 	"github.com/amryamanah/go-boilerplate/pkg/config"
 	"github.com/amryamanah/go-boilerplate/pkg/logger"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
-	"log"
-)
-
-type AppEnv string
-
-const (
-	DEV  AppEnv = "dev"
-	TEST        = "test"
-	PROD        = "prod"
 )
 
 type Application struct {
@@ -40,20 +30,10 @@ func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
 
-
-
 func (a *Application) Start() error {
 	address := fmt.Sprintf("0.0.0.0:%s", config.Config.GetApiPort())
 	logger.Info.Printf("starting server at %s", address)
 	return a.router.Run(address)
-}
-
-func (a *Application) InitStore() {
-	conn, err := sql.Open("postgres", config.Config.GetDBConnStr())
-	if err != nil {
-		log.Fatal("cannot connect to db:", err)
-	}
-	a.Store = store.NewStore(conn)
 }
 
 func (a *Application) Close() error {
